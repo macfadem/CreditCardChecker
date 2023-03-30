ede96daa2a15c03e4189e33de5fff291c6aa74c7
Kevin Macfadem <k_macfadem@hotmail.com>
2023-03-30 17:11:32 -0300

    Basic CreditCardChecker

---
+++ main.js
@@ -0,0 +1,31 @@
+validateCreditCard = (cardNumber) => {
+    let sum = 0;
+    let shouldDouble = false;
+    for (let i = cardNumber.length - 1; i >= 0; i--) {
+        let digit = parseInt(cardNumber[i]);
+        if (shouldDouble) {
+            if ((digit *= 2) > 9) {
+                digit -= 9;
+            }
+        }
+        sum += digit;
+        shouldDouble = !shouldDouble;
+    }
+    return sum % 10 === 0;
+};
+
+const cardNumberInput = document.getElementById('cardNumber');
+  const submitBtn = document.getElementById('submitBtn');
+  
+  submitBtn.addEventListener('click', (event) => {
+    event.preventDefault(); // Prevent the form from submitting
+    
+    const cardNumber = cardNumberInput.value;
+    const isValid = validateCreditCard(cardNumber);
+    
+    if (isValid) {
+      alert('Credit card is valid!');
+    } else {
+      alert('Invalid credit card number');
+    }
+  });
